0855ad0fdf330d46772627338f59cdeba49ca74d
Anne Ensign <anne.ensign@gmail.com>
2021-10-26 12:30:50 -0400

    Add staticmethod

--- shipping/shipping.py
+++ shipping/shipping.py
@@ -2,14 +2,13 @@ class ShippingContainer:
 
 	next_serial = 1337
 
-	def __init__(self, owner_code, contents):
-		self.owner_code = owner_code # Instance attributes (self)
-		self.contents = contents
-		self.serial = ShippingContainer.next_serial # Class attributes
+	@staticmethod
+	def _generate_serial(): # One underscore, implementation detail within the class
+		result = ShippingContainer.next_serial
 		ShippingContainer.next_serial += 1
-		
-#  If we attempted to assign to an existing class attribute 
-# through the self reference, we would actually 
-# create a new instance attribute, 
-# which would hide the class attribute, 
-# and the class attribute would remain unmodified.
+		return result
+
+	def __init__(self, owner_code, contents):
+		self.owner_code = owner_code 
+		self.contents = contents
+		self.serial = ShippingContainer._generate_serial()
