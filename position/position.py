363c60d7bff195c1f5f8dbe1173028a9317ff012
Anne Ensign <anne.ensign@gmail.com>
2021-10-29 15:35:18 -0400

    Add position folder and file

---
+++ position/position.py
@@ -0,0 +1,23 @@
+class Position:
+
+	def __init__(self, latitude, longitude):
+		if not(-90 <= latitude <= +90):
+			raise ValueError(f"Latitude {latitude} out of range")
+
+		if not (-180 <= longitude <= +180):
+			raise ValueError(f"Longitude {longitude} is out of range")
+		
+		self._latitude = latitude
+		self._longitude = longitude
+	
+	@property
+	def latitude(self):
+		return self._latitude
+	
+	@property
+	def longitude(self):
+		return self._longitude
+
+	# For new representation
+	def __repr__(self):
+		return f"Position {self.latitude} {self.longitude}"
