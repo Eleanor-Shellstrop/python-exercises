7166bdcb1358a0971ae0b3037db6284c9f3a5855
Anne Ensign <anne.ensign@gmail.com>
2021-11-02 16:19:22 -0400

    ssg.py typer on main

--- static-site-generator/ssg/site.py
+++ static-site-generator/ssg/site.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 class Site:
 	def __init__(self, source, dest):
-		self._source = Path(source)
-		self._dest = Path(dest)
+		self.source = Path(source)
+		self.dest = Path(dest)
 	
 	def create_dir(self, path):
 		directory = self.dest / path.relative_to(self.source)
@@ -13,5 +13,5 @@ class Site:
 	def build(self):
 		self.dest.mkdir(parents=True, exist_ok=True)
 		for path in self.source.rglob("*"):
-			if self.path.isdir(path):
+			if path.is_dir(path):
 				self.create_dir(path)
