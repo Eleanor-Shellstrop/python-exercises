3a85c633e849c4358f2ad33136c7c1aa7b67e217
Anne Ensign <anne.ensign@gmail.com>
2021-11-03 10:46:11 -0400

    Start section 2, Parser Class

--- static-site-generator/ssg.py
+++ static-site-generator/ssg.py
@@ -1,10 +1,14 @@
 import typer
-
 from ssg.site import Site
 
+import ssg.parsers
 
 def main(source="content", dest="dist"):
-	config = {"source": source, "dest": dest}
+	config = {
+		"source": source, 
+		"dest": dest, 
+		"parsers": [ssg.parsers.REsourceParser(),],
+	}
 	Site(**config).build()
 
 typer.run(main)
