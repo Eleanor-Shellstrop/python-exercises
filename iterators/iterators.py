fb6bb3ce290a2476b537f2d8108d642267c914c5
Anne Ensign <anne.ensign@gmail.com>
2021-11-05 10:36:46 -0400

    Add depth-first, in-order iterator

--- iterators/iterators.py
+++ iterators/iterators.py
@@ -75,7 +75,36 @@ class PreOrderIterator:
 		return self
 
 
-# repl example:
-# expr_tree = "* + - a b c d".split()
-# iterator = PreOrderIterator(expr_tree)
-# " ".join(iterator)  	// returns '* + a b - c d'
+class InOrderIterator:
+	def __init__(self, sequence):
+		if not _is_perfect_length(sequence):
+			raise ValueError(
+				f"Sequence of length {len(sequence)} does not represent "
+				"a perfect binary tree with length 2n -1"
+			)
+		self._sequence = sequence
+		self._stack = []
+		self._index = 0
+
+	def __next__(self):
+		if (len(self._stack) == 0) and (self._index >= len(self._sequence)):
+			raise StopIteration
+		
+		# Push left children onto the stack while possible
+		while self._index < len(self._sequence):
+			self._stack.append(self._index)
+			self._index = _left_child(self._index)
+		
+		# Pop from stack and process, before moving tot he right child
+		index = self._stack.pop()
+		result = self._sequence[index]
+		self._index = _right_child(index)
+		return result
+
+	def __iter__(self):
+		return self
+
+# Repl example:
+# expr_tree = ["*", "+", "-", "a", "b", "c", "d"]
+# iterator = InOrderIterator(expr_tree)
+# " ".join(iterator) 	\\ Returns 'a + b * c - d'
