e8f54987144c193367882f095e740c3d8d98d8e2
Anne Ensign <anne.ensign@gmail.com>
2021-11-05 09:48:10 -0400

    Add Depth-first, pre-order iterator

--- iterators/iterators.py
+++ iterators/iterators.py
@@ -5,8 +5,6 @@ def _is_perfect_length(sequence):
 	n = len(sequence)
 	return ((n + 1) & n == 0) and (n != 0)
 
-# test in repl for length:
-# {i: _is_perfect_length(["x"] * i) for i in range(0, 32)}
 
 class LevelOrderIterator:
 
@@ -29,4 +27,55 @@ class LevelOrderIterator:
 	def __iter__(self):
 		return self
 
+
 # expr_tree = ["*", "+", "-", "a", "b", "c", "d"]
+
+
+# functions to give index respective of parent
+def _left_child(index):
+	return 2 * index + 1
+
+def _right_child(index):
+	return 2 * index + 2
+
+
+class PreOrderIterator:
+	def __init__(self, sequence):
+		if not _is_perfect_length(sequence):
+			raise ValueError(
+				f"Sequence of length {len(sequence)} does not represent "
+				"a perfect binary tree with length 2n -1"
+			)
+		self._sequence = sequence
+		# Start at root node
+		self._stack = [0]
+	
+	def __next__(self):
+		# Could test for truthiness, but explicit test 
+		# against length better states intent 
+		if len(self._stack) == 0:
+			raise StopIteration
+		
+		index = self._stack.pop()
+		result = self._sequence[index]
+
+		# Pre-order: Push right child first so left child is 
+		# popped and processed first. Last-in, first-out
+		right_child_index = _right_child(index)
+		if right_child_index < len(self._sequence):
+			self._stack.append(right_child_index)
+
+		left_child_index = _left_child(index)
+		if left_child_index < len(self._sequence):
+			self._stack.append(left_child_index)
+		
+		return result
+	
+	def __iter__(self):
+		return self
+
+
+# repl example:
+# expr_tree = "* + - a b c d".split()
+# iterator = PreOrderIterator(expr_tree)
+# " ".join(iterator)  	// returns '* + a b - c d'
