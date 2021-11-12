2f94ae11914bc60d8fe63978cac0cad67ac19671
Anne Ensign <anne.ensign@gmail.com>
2021-11-12 10:47:18 -0500

    Change to contextmanager decorator

--- error_handling/cm.py
+++ error_handling/cm.py
@@ -1,9 +1,14 @@
-class LoggingContextManager:
-	def __enter__(self):
-		print('LoggingContextManager.__enter__()')
-		return "You're in a with-block!"
-	
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		print('LoggingContextManager.__exit__({}, {}, {})'.format(
-			exc_type, exc_val, exc_tb))
-		return
+import contextlib
+import sys
+
+
+@contextlib.contextmanager
+def logging_context_manager():
+	print('logging_context_manger: enter')
+	try:
+		yield "You're in a with block!"
+		print('logging_context_manager: normal exit')
+	except Exception:
+		print('logging_context_manager: exceptional exit',
+			sys.exc_info())
+		raise
