76b63b1e44a615441eda493d62aed8b8b3ed6bb7
Anne Ensign <anne.ensign@gmail.com>
2021-11-15 13:24:53 -0500

    Add DateReminder class

--- pluralsight-projects-python-build-cli-planner-app-a582783/src/deadlined_reminders.py
+++ pluralsight-projects-python-build-cli-planner-app-a582783/src/deadlined_reminders.py
@@ -1,4 +1,7 @@
 from abc import ABC, ABCMeta, abstractmethod
+from dateutil.parser import parse
+from datetime import datetime
+
 
 from collections.abc import Iterable
 
@@ -15,3 +18,16 @@ class DeadlinedReminder(Iterable, ABC):
 	@abstractmethod
 	def is_due(self):
 		pass
+
+
+class DateReminder(DeadlinedReminder):
+	
+	def __init__(self, text, date):
+		self.date = parse(date, dayfirst=True)
+		self.text = text
+	
+	def __iter__(self):
+		return iter([self.text, self.date.isoformat()])
+
+	def is_due(self):
+		return self.date < datetime.now()
